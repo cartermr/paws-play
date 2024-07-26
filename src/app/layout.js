08d63cfea268bfede0550e22b7cd29470c538180
Matt <carter.matthew@icloud.com>
2024-07-26 00:19:46 -0400

    Front page initial done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,13 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import 'primereact/resources/themes/lara-light-cyan/theme.css'
+import 'primeflex/primeflex.css'
+import { Toolbar } from 'primereact/toolbar'
 
 const inter = Inter({ subsets: ["latin"] });
+
+const centerToolbar = (
+  <span className="text-2xl">Paws & Play</span>
+)
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +17,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Toolbar center={centerToolbar} />
+        {children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,48 @@
-import Image from "next/image";
+import 'primereact/resources/themes/lara-light-cyan/theme.css'
+import Link from 'next/link'
+import { Image } from 'primereact/image'
+import { Avatar } from 'primereact/avatar'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
+   <main className='flex flex-column md:flex-row'>
+    <div className='p-1 md:p-4 mb-3 md:mb-0'>
+      <section>
+        <span className='block text-center md:text-left text-6xl font-bold mb-3'>Paws & Plays</span>
+        <p className='mt-0 mb-4 text-700 line-height-3'>
+          Paws and Play is a pet sitting service to ease the anxiety on you and your pets while you’re away. Have fast paced lives with no time to walk your pet between activities? I offer walks. Mom and dad need some time off? I also babysit.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <span className='block text-3xl font-bold'>Services</span>
+        <ul className='mt-2'>
+          <li>
+            <span className='font-bold'>Pet Sitting</span>
+            <ul>
+              <li>$10.00 per visit</li>
+              <li>Pet sitting includes playtime, a walk, feeding, locking your pet back up if needed, and/or any special requests. I will also send you a text with an update on your pet.</li>
+            </ul>
+          </li>
+          <li className='mt-1'>
+            <span className='font-bold'>Dog Walking</span>
+            <ul>
+              <li>$5.00 per visit</li>
+              <li>Dog walking can be provided separate from pet sitting.</li>
+            </ul>
+          </li>
+          <li className='mt-1'>
+            <span className='font-bold'>Babysitting</span>
+            <ul>
+              <li>$10.00 an hour for 1 - 2 children.</li>
+            </ul>
+          </li>
+        </ul>
+        <Link className='mr-3 p-button' href='/'>Contact Me</Link>
+        <Link className='p-button' href='/'>About Me</Link>
+      </section>
+    </div>
+    <div className='flex flex-column xl:flex-row align-items-center justify-content-around	pt-4 xl:pt-0'>
+      <Image src='/images/cookie.jpg' className='mx-2' alt="Image of dog" height='400' />
+      <Image src='/images/avery.jpg' alt="Image of dog" height='400' />
+    </div>
+   </main>
   );
 }
